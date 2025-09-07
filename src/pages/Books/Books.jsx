d4cd4aef2ce12c50ff5eeafed617d5d922a134ad
Jubaer Hossen <jubaerhossen042@gmail.com>
2025-09-07 14:47:20 +0600

    added book card

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
+import Book from '../Book/Book';
 
 
 
-const Books = () => {
+const Books = ({data}) => {
   const [allBooks, setAllBooks] = useState([]);
 
-useEffect(()=>{
-  fetch("booksData.json")
-  .then(res => res.json())
-  .then(data =>{
-    console.log(data);
-  })
-},[])
+// useEffect(()=>{
+//   fetch("booksData.json")
+//   .then(res => res.json())
+//   .then(data =>{
+//     console.log(data);
+//   })
+// },[]);
+
+// const bookPromise = fetch('./booksData.json').then(res=>res.json())
+
 
   return (
     <div>
       <h3>hello ami books</h3>
+      <Suspense fallback={<span>loading...</span>}>
+       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+         {
+          data.map((singleBook)=><Book key={singleBook.bookId} singleBook={singleBook}></Book>)
+        }
+       </div>
+      </Suspense>
     </div>
   );
 };

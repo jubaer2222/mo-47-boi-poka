c6b50a28106853fa76994522d511284322950b08
Jubaer Hossen <jubaerhossen042@gmail.com>
2025-09-08 14:39:26 +0600

    added book details route

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,30 +1,41 @@
 import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
+import { Link } from 'react-router';
 
 const Book = ({ singleBook }) => {
   // const data = use(bookPromise);
   // console.log(data);
-  const {bookName,author,image}=singleBook;
+  const {bookName,author,image,rating,category,tags,yearOfPublishing,publisher,bookId}=singleBook;
   return (
+    <Link to={`/bookDetails/${bookId}`}>
     <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure className='p-6'>
+      <div className="card bg-base-100 w-96 shadow-sm border p-6">
+        <figure className='p-5 bg-gray-100 w-2/3 mx-auto'>
           <img
+            className='h-[166px]'
             src={image}
             alt="Shoes" />
         </figure>
         <div className="card-body">
+          <div className='flex justify-center gap-3'>
+            {
+              tags.map(tag=><button>{tag}</button>)
+            }
+          </div>
           <h2 className="card-title">
-            Card Title
-            <div className="badge badge-secondary">NEW</div>
+            {bookName}
+            <div className="badge badge-secondary">{yearOfPublishing}</div>
           </h2>
-          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <p>Book by : {publisher}</p>
+
           <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
+            <div className="badge badge-outline">{category}</div>
+            <div className="badge badge-outline">{rating}<FaStarHalfAlt /></div>
           </div>
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
